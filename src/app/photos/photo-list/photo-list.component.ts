8111237a6b6f48f44f951f207409315da99c6e17
Aline Athaydes <lyne.argolo@gmail.com>
2022-04-25 21:20:51 -0300

    estudando angular em Fundamentos2

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
+import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
@@ -7,22 +11,39 @@ import { PhotoService } from '../photo/photo.service';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy{
 
-  photos: any[] = [];
+  photos: Photo[] = [];
+  filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
 
   constructor(
-    private photoService: PhotoService,
-    private activatedRouter: ActivatedRoute
-    ){  
-}
+    private activatedRouter: ActivatedRoute,
+    private photoService: PhotoService
+  ){ }
+  
   ngOnInit(): void {
-    const userName = this.activatedRouter.snapshot.params.userName;
-    this.photoService
-      .listFromUser(userName)
-      .subscribe(photos => this.photos = photos);
-
+    this.userName = this.activatedRouter.snapshot.params.userName;
+    this.photos = this.activatedRouter.snapshot.data['photos'];
+    this.debounce
+    .pipe(debounceTime(300))
+    .subscribe(filter => this.filter = filter);
   }
  
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
+  }
+
+  load(){
+    this.photoService
+      .listFromUserPaginated(this.userName, ++this.currentPage)
+      .subscribe(photos => {
+        this.photos = this.photos.concat(photos);
+        if(!photos.length) this.hasMore = false;
+      })
+  }
 
 }
